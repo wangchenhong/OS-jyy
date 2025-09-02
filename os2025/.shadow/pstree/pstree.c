0b537c5fae04e2ae67465962004265d4be06a4a7
tracer-nju <tracer@nju.edu.cn>
2025-09-02 08:08:36 +0800

    Linux Slave1 3.10.0-1160.119.1.el7.x86_64 #1 SMP Tue Jun 4 14:43:51 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  08:08:36 up 3 days, 22:47,  0 users,  load average: 0.08, 0.06, 0.18

--- os2025/.shadow/pstree/pstree.c
+++ os2025/.shadow/pstree/pstree.c
@@ -72,6 +72,120 @@ ProcessNode* hash_table_lookup(HashTable *ht, pid_t pid){
     return NULL;
 }
 
+int read_process_info(pid_t pid, pid_t *ppid, char *name, size_t name_size){
+    char path[60];
+    snprintf(path, sizeof(path), "/proc/%d/stat", pid);
+
+    FILE *fp = fopen(path, "r");
+    if(!fp){
+        return -1;
+    }
+
+    char buffer[1024];
+    if(fgets(buffer, sizeof(buffer), fp) == NULL){
+        fclose(fp);
+        return -1;
+    }
+    fclose(fp);
+
+    //查找进程名
+    char *start = strchr(buffer, '(');
+    char *end = strchr(buffer, ')');
+    
+    if(!start || !end || start >= end){
+        return -1;
+    }
+
+    *start++;
+    size_t name_len = end - start;
+    if(name_len > name_size - 1){
+        name_len = name_size -1;    //防止缓冲区溢出
+    }
+
+    strncpy(name, start, name_len);
+    name[name_len] = '\0';
+
+    //跳过空格和进程状态字段
+    char *p = end + 1;
+    while(*p == ' ') p++;
+    p = strchr(p, ' ');
+    if(!p) return -1;
+    p++;
+
+    *ppid = atoi(p);
+
+    return 0;
+
+}
+
+//打印单个进程节点及其子节点
+void print_node(ProcessNode *node, int depth, int is_last){
+    for(int i = 0; i < depth; i++){
+        if(i == depth -1){
+            printf(is_last ? "'--" : "|--");
+        }else{
+            printf("    ");
+        }
+    }
+    printf("%s(%d)\n", node->name, node->pid);
+
+    ProcessNode *child = node->children;
+    while(child != NULL){
+        int child_is_last = (child->sibling == NULL);
+        print_node(child, depth + 1, child_is_last);
+        child = child->sibling;
+    }
+}
+
+void print_process_tree(ProcessNode *root){
+    if(root == NULL){
+        printf("没有找到进程树\n");
+        return ;
+    }
+    printf("%s(%d)\n", root->name, root->pid);
+
+    //递归打印子节点
+    ProcessNode *child = root->children;
+    while(child != NULL){
+        int child_is_last = (child->sibling == NULL);
+        print_node(child, 1, child_is_last);
+        child = child->sibling;
+    }
+}
+
+void free_process_node(ProcessNode *node){
+    if(node == NULL){
+        return ;
+    }
+    ProcessNode *child = node->children;
+    while(child != NULL){
+        ProcessNode *next = child->sibling;
+        free_process_node(child);
+        child = next;
+    }
+    free(node);
+}
+
+void free_resources(HashTable *ht){
+    if(ht == NULL){
+        return;
+    }
+
+    for(size_t i = 0; i < ht->size; ++i){
+        HashNode *hash_node = ht->buckets[i];
+        while(hash_node != NULL){
+            free_process_node(hash_node->process);
+            hash_node->process = NULL;  //避免悬空指针
+
+            HashNode *next = hash_node->next;
+            free(hash_node);
+            hash_node= next;
+        }
+        ht->buckets[i] = NULL;
+    }
+    free(ht->buckets);
+    free(ht);
+}
 
 
 int main() {
@@ -90,13 +204,53 @@ int main() {
         if(entry->d_type == DT_DIR && is_numeric(entry->d_name)){
             pid_t pid = atoi(entry->d_name);
             
-            // pid_t ppid = read_ppid(pid);
-            // char *name = read_process_name(pid);
+            pid_t ppid;
+            char *name[256];
 
-            
+            if(read_process_info(pid, ppid, name, sizeof(name)) != 0){
+                continue;
+            }
 
-            printf("%s\n", entry->d_name);
+            ProcessNode *node = malloc(sizeof(ProcessNode));
+            node->pid = pid;
+            node->ppid = ppid;
+            strncpy(node->name, name, sizeof(node->name));
+            node->parent = NULL;
+            node->children = NULL;
+            node->sibling = NULL;
+
+            hash_table_insert(ht, pid, node);
+
+            free(name);
         }
     }
     closedir(proc_dir);
+
+    //建立父子关系
+    //遍历哈下表的所有桶
+    for(size_t i = 0; i < ht->size; i++){
+        HashNode *hashnode = ht->buckets[i];
+        while(hashnode != NULL){
+            ProcessNode *node = hashnode->process;
+
+            if(node->ppid != 0){
+                ProcessNode *parent = hash_table_lookup(ht, node->ppid);
+
+                if(parent != NULL){
+                    node->parent = parent;
+                    node->sibling = parent->children;
+                    parent->children = node;
+                }
+            }
+            hashnode = hashnode->next;
+        }
+    }
+
+    ProcessNode *root = hash_table_lookup(ht, 1);
+    if(root != NULL){
+        print_process_tree(root);
+    }
+    free_resources(ht);
+
+    return 0;
 }
